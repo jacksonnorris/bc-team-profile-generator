0503c7c8604a2f43058aa6162f28a2f3c8183f62
jacksonnorris <jacksonnorr@gmail.com>
2022-03-22 14:15:59 -0400

    added fs and ability to generate webpages based on employee type

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Webpage = require('./lib/Webpage')
 
 const employeeList = []; 
 
-const engineer = new Engineer('Jim', 1, 'j@gmail.com');
+const engineer = new Engineer('Jim', 1, 'j@gmail.com', 'Jimzo213');
 const intern = new Intern('Jacob', 2, 'jacob@gmail.com', 'UNC Charlotte');
 const employee = new Employee('James', 3, 'james@gmail.com');
 const manager = new Manager('Jack', 4, 'jack@gmail.com', 1);
