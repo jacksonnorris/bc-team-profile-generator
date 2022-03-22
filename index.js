25d32beca3c81876ad24d5058b67307db9cb3bfc
jacksonnorris <jacksonnorr@gmail.com>
2022-03-22 14:23:56 -0400

    fixed positioning of writeFile

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const employeeList = [];
 
 const engineer = new Engineer('Jim', 1, 'j@gmail.com', 'Jimzo213');
 const intern = new Intern('Jacob', 2, 'jacob@gmail.com', 'UNC Charlotte');
-const employee = new Employee('James', 3, 'james@gmail.com');
+const employee = new Employee('James', 3, 'james@gmail.com', 'Employee');
 const manager = new Manager('Jack', 4, 'jack@gmail.com', 1);
 
 employeeList.push(engineer, intern, employee, manager);
 
 const webpage = new Webpage(employeeList);
 
-console.log(webpage.generateWebpage());
+// console.log(webpage.generateWebpage());
+webpage.generateWebpage();
