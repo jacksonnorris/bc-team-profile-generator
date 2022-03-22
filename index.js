58fbdbda5b062d055c6ddd8b45453535b0a5f1af
jacksonnorris <jacksonnorr@gmail.com>
2022-03-22 15:20:19 -0400

    added conditional features for prompt and looping ability

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Employee = require('./lib/Employee.js');
 const Manager = require('./lib/Manager');
 const Webpage = require('./lib/Webpage')
 const inquirer = require('inquirer');
+const { listenerCount } = require('process');
 
 const employeeList = []; 
 
@@ -19,34 +20,69 @@ const employeeList = [];
 // console.log(webpage.generateWebpage());
 // webpage.generateWebpage();
 
-const employeeTypes = ['Engineer', 'Intern', 'Manager'];
+var addMore = true;
+
+const employeeTypes = ['Engineer', 'Intern', 'Manager', 'Done Adding'];
 const employeeQuestions = ['What is their GitHub username?', 'What School do they go to?', 'What is their office number?'];
 
-inquirer.prompt([
-    {
-        type: 'list',
-        message: 'What type of Employee?',
-        name: 'employeeType',
-        choices: employeeTypes,
-    },
-    {
-        type: 'input',
-        message: 'What is the employee name?',
-        name: 'name'
-    },
-    {
-        type: 'input',
-        message: 'What is the employee email?',
-        name: 'email'
-    },
-    {
-        type: 'input',
-        message: 'What is the employee id?',
-        name: 'id'
-    },
+const employeeType = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: `What is the role of the employee?`,
+            name: 'employeeType',
+            choices: employeeTypes,
+        }, 
+        
+    ]).then((employee) => {
+        if (employee.employeeType === 'Engineer') {
+            uniquePrompt(employeeQuestions[0]);
+        }
     
-]).then((answers) => {
+    
+        // const webpage = new Webpage(employeeList);
+    })
+}
+const plainPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the employee?',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'What is the email of the employee?',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'What is the id of the employee?',
+            name: 'id'
+        },
+    
+    ]).then((answers) => {
+        employeeType();
 
-
-    // const webpage = new Webpage(employeeList);
-})
+        // const webpage = new Webpage(employeeList);
+    })
+}
+const uniquePrompt = (msg) => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: msg,
+            name: 'uniqueAttr'
+        },
+        {
+            type: 'list',
+            message: 'Did you want to add any more employees?',
+            name: 'continue',
+            choices: ['Yes', 'No']
+        }
+    ]).then((cont) => {
+        if (cont.continue === 'No') { addMore = false; }
+        else {  plainPrompt(); }
+    })
+}
+plainPrompt();
